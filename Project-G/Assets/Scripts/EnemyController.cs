7a42f9d5fc4158502b74b99d491deb6f35ec69c6
İskender <akyuziskender@hotmail.com>
2020-06-24 18:59:18 +0300

    RandomPoster on walls
    + 25% chance of putting a poster
    + Poster's sortingOrder was assigned

--- Project-G/Assets/Scripts/EnemyController.cs
+++ Project-G/Assets/Scripts/EnemyController.cs
@@ -15,14 +15,15 @@ public class EnemyController : MonoBehaviour
 
 	public DamageHelper DamageHelper;
 	private Animator _animator;
-
-	public bool IsRunning = false;
 	private Vector2 _sightDir;
 	private float _sightRange = 5f;
 	private bool _isAttacking = false;
+	private bool _isRunning = false;
 	[SerializeField] private float _attackRange = 0.4f;
 
 	public EnemyCloseRangeAttackController AttackController;
+
+	public bool IsRunning { get { return _isRunning; } }
 
 	// Start is called before the first frame update
 	void Start() {
@@ -71,20 +72,15 @@ public class EnemyController : MonoBehaviour
 
 			if (_targetPos != _nullVector) {
 				transform.position = Vector3.MoveTowards(transform.position, _targetPos, Time.deltaTime * _speed);      			// moving the enemy towards to target
-				if(Vector3.Distance(transform.position, _targetPos) != 0) {
-					IsRunning = true;
-				}
-				else {
-					IsRunning = false;
-				}
+				_isRunning = (Vector3.Distance(transform.position, _targetPos) != 0);
 			}
 			else {
-				IsRunning = false;
+				_isRunning = false;
 			}
 		}
 		else {		// if the player is dead, stop attacking
 			_isAttacking = false;
-			IsRunning = false;
+			_isRunning = false;
 		}
 	}
 
@@ -100,7 +96,7 @@ public class EnemyController : MonoBehaviour
 	}
 
 	private void EnemyAnimate() {
-		_animator.SetBool("IsRunning", IsRunning);
+		_animator.SetBool("IsRunning", _isRunning);
 		_animator.SetBool("IsAttacking", _isAttacking);
 		_animator.SetFloat("Horizontal", _sightDir.x); 
 		_animator.SetFloat("Vertical", _sightDir.y);
