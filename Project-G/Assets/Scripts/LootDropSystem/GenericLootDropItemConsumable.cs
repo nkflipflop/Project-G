bbc0eaa11309c988b47f2cb94dbbd28f9838e695
İskender <akyuziskender@hotmail.com>
2020-07-10 22:35:43 +0300

    v1.1

---
+++ Project-G/Assets/Scripts/LootDropSystem/GenericLootDropItemConsumable.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When we're inheriting we have to insert Consumable as a type to GenericLootDropItem
+/// </summary>
+[System.Serializable]
+public class GenericLootDropItemConsumable : GenericLootDropItem<Consumable> {
+}
