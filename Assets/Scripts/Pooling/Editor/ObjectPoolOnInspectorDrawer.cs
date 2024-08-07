ffcbebd527bee754a58cef59fc7106b048bded65
İskender <>
2024-08-07 20:17:02 +0300

    ~ ObjectPoolOnInspector has been improved.

--- Assets/Scripts/Pooling/Editor/ObjectPoolOnInspectorDrawer.cs
+++ Assets/Scripts/Pooling/Editor/ObjectPoolOnInspectorDrawer.cs
@@ -12,36 +12,45 @@ namespace Pooling.Editor
             ObjectPoolOnInspector pool = (ObjectPoolOnInspector)PropertyUtility.GetTargetObjectOfProperty(property);
             pool?.Refresh();
             
-            EditorGUI.BeginProperty(position, label, property);
+            position.height = 16;
+            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label, toggleOnLabelClick: true);
 
-            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            if (property.isExpanded)
+            {
+                EditorGUI.BeginProperty(position, label, property);
 
-            SerializedProperty activeObjects = property.FindPropertyRelative("activeObjects");
-            SerializedProperty inActiveObjects = property.FindPropertyRelative("inActiveObjects");
+                SerializedProperty activeObjects = property.FindPropertyRelative("activeObjects");
+                SerializedProperty inActiveObjects = property.FindPropertyRelative("inActiveObjects");
 
-            Rect inActiveObjectsPos = new Rect(position.x,
-                position.y + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing,
-                position.width, EditorGUIUtility.singleLineHeight);
-            Rect activeObjectsPos = new Rect(position.x,
-                inActiveObjectsPos.y + EditorGUIUtility.standardVerticalSpacing +
-                EditorGUI.GetPropertyHeight(inActiveObjects), position.width, EditorGUIUtility.singleLineHeight);
+                Rect inActiveObjectsPos = new Rect(position.x,
+                    position.y + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing,
+                    position.width, EditorGUIUtility.singleLineHeight);
+                Rect activeObjectsPos = new Rect(position.x,
+                    inActiveObjectsPos.y + EditorGUIUtility.standardVerticalSpacing +
+                    EditorGUI.GetPropertyHeight(inActiveObjects), position.width, EditorGUIUtility.singleLineHeight);
 
-            int cachedIndent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
+                int cachedIndent = EditorGUI.indentLevel;
+                EditorGUI.indentLevel++;
 
-            EditorGUI.PropertyField(inActiveObjectsPos, inActiveObjects, new GUIContent("In-Active"));
+                EditorGUI.PropertyField(inActiveObjectsPos, inActiveObjects, new GUIContent("In-Active"));
             
-            Color cachedColor = GUI.backgroundColor;
-            GUI.backgroundColor = Color.green;
-            EditorGUI.PropertyField(activeObjectsPos, activeObjects, new GUIContent("Active"));
-            GUI.backgroundColor = cachedColor;
+                Color cachedColor = GUI.backgroundColor;
+                GUI.backgroundColor = Color.green;
+                EditorGUI.PropertyField(activeObjectsPos, activeObjects, new GUIContent("Active"));
+                GUI.backgroundColor = cachedColor;
             
-            EditorGUI.indentLevel = cachedIndent;
-            EditorGUI.EndProperty();
+                EditorGUI.indentLevel = cachedIndent;
+                EditorGUI.EndProperty();
+            }
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            if (!property.isExpanded)
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
+            
             SerializedProperty passiveObjects = property.FindPropertyRelative("inActiveObjects");
             SerializedProperty activeObjects = property.FindPropertyRelative("activeObjects");
             return EditorGUIUtility.singleLineHeight + (3 * EditorGUIUtility.standardVerticalSpacing) +
